2a82d5ec9894a1cece5d8597ed7aa51ac02db3b3
StMrr <boldeagle175@gmail.com>
2021-02-07 13:50:05 -0500

    added all the files for Task B

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "funcs.h"
+
+int main(){
+	std::cout << "L = 5, U = 10" << "\n";
+	print_interval(5, 10);
+	std::cout << "L = 29, U = 43" << "\n";
+	print_interval(29, 43);
+
+}
